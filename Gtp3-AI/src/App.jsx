bc189857eb3eb2e14d2ebd3b724bea3a9a1b5f56
Dylan Chaves <Dylanpotoy@gmail.com>
2023-04-19 19:36:40 -0600

    chatgpt

--- Gtp3-AI/src/App.jsx
+++ Gtp3-AI/src/App.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
+import './App.css'
 import { Brand, CTA, Navbar } from './componets'
 import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from './containers'
 
 const App = () => {
   return (
     <div className='App'>
-    <div className='gradient__bg'>
-      <Navbar/>
-      <Header/>
+      <div className='gradient__bg'>
+        <Navbar/>
+        <Header/>
 
     </div>
-
+    <Brand/>
+    <WhatGPT3/>
+    <Features/>
+    <Possibility/>
+    <CTA></CTA>
+    <Blog></Blog>
+    <Footer></Footer>
     </div>
 
   )
