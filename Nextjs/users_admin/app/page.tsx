8244b9a638235a865ca44e9cc987246dab7bdd7f
Dylan Chaves <Dylanpotoy@gmail.com>
2023-04-19 23:10:30 -0600

    fetching data

--- Nextjs/users_admin/app/page.tsx
+++ Nextjs/users_admin/app/page.tsx
@@ -1,9 +1,37 @@
+import Image from "next/image";
 
-export default function Home() {
+async function fechUsers(id: string) {
+  let url = id
+    ? `http://127.0.0.1:8080/users/${_id}`
+    : "http://127.0.0.1:8080/users";
+  const datos = await fetch(url);
+  const res = await datos.json();
+  return res
+}
+
+export default async function Home() {
+  const users = await fechUsers();
   return (
     <div>
       <h1>Home page</h1>
-      <p>esta es la homeee</p>
+
+      <ul>
+        {
+          users.map((user: any) => (
+            <li key={user.id}>
+              <div>
+
+                <h5>{user.name} {user.lastname}</h5>
+
+                <p>Tienes:
+                  {user.Citas.length} <strong>citas pendientes</strong></p>
+              </div>
+              <img src="profile_user.jpg" alt="userProfile" width={200} height={40} />
+            </li>
+          ))
+        }
+
+      </ul>
     </div>
   )
 }
--- Nextjs/users_admin/next.config.js
+++ Nextjs/users_admin/next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.vecteezy.com",
+        port: "",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
